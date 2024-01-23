8f70ffd4f71fa436c753eca6118e5a7970bfea5e
Rusen Altindag <r.altindag95@gmail.com>
2024-01-23 12:42:09 +0100

    add allure

--- test_image_preview.py
+++ test_image_preview.py
@@ -11,20 +11,29 @@ from appium import webdriver
 from appium.options.mac import Mac2Options
 from appium.webdriver.common.appiumby import AppiumBy
 
+import allure
+from allure import attachment_type
 
-@pytest.fixture()
+
+@pytest.fixture(scope="session")
 def driver():
     options = Mac2Options()
     options.bundle_id = "com.apple.Preview"
     drv = webdriver.Remote("http://127.0.0.1:4723", options=options)
+    None if os.path.exists("output") else os.mkdir("output")
     yield drv
     drv.quit()
 
 
+@allure.title(
+    "Start any Image Editing application for desktop that can import & export images with different formats (ex. Paint)"
+)
+@allure.description("Open Preview App on MAC")
 def test_open_image_editor(driver):
     pass
 
 
+@allure.title("Import Image: IMAGE_1")
 def test_import_image(driver):
     driver.find_element(
         by=AppiumBy.IOS_PREDICATE, value="elementType == 56 AND title == 'File'"
@@ -45,13 +54,45 @@ def test_import_image(driver):
             ]
         },
     )
-    image_path = "/Users/rusen/chaos/IMAGE_1.png"
+    image_path = os.getcwd() + "/IMAGE_1.png"
     driver.execute_script("macos:keys", {"keys": [*image_path]})
     driver.execute_script("macos:keys", {"keys": ["XCUIKeyboardKeyEnter"]})
     driver.execute_script("macos:keys", {"keys": ["XCUIKeyboardKeyEnter"]})
 
     # TODO: Verify with image comparison that the imported Image looks correctly in the Image Editor
+    display_image = driver.find_element(
+        by=AppiumBy.IOS_PREDICATE, value="elementType == 46"
+    )
+    display_image.screenshot("IMAGE_1_preview_ss_element_type.png")
 
+    display_image = driver.find_element(
+        by=AppiumBy.CLASS_NAME, value="XCUIElementTypeImage"
+    )
+
+    display_image.screenshot("IMAGE_1_preview_ss_class_name.png")
+
+
+@allure.title(
+    "Verify with image comparison that the imported Image looks correctly in the Image Editor"
+)
+def test_preview_of_image_with_original(driver):
+    original_img = Image.open("IMAGE_1.png")
+    preview_img = Image.open("IMAGE_1_preview_ss_class_name.png")
+
+    preview_img = preview_img.resize(original_img.size)
+    img_diff = Image.new("RGBA", original_img.size)
+    mismatch = pixelmatch(original_img, preview_img, img_diff)
+    img_diff.save("original_vs_preview_diff.png")
+    allure.attach.file(
+        "original_vs_preview_diff.png",
+        name="Differences between Preview and Original IMG",
+        attachment_type=attachment_type.PNG,
+    )
+    assert mismatch == 0
+
+
+@allure.title("Export the Image in JPG format to a local drive")
+def test_export_func(driver):
     driver.find_element(
         by=AppiumBy.IOS_PREDICATE, value="elementType == 56 AND title == 'File'"
     ).click()
@@ -82,10 +123,14 @@ def test_import_image(driver):
     save.click()
 
 
+@allure.title("Verify that the exported image exists")
 def test_verify_that_the_exported_image_exists(driver):
     assert os.path.exists("IMAGE_1.jpg")
 
 
+@allure.title(
+    "Verify with image comparison whether the exported image is equal to IMAGE_2"
+)
 def test_verify_with_image_comparison_whether_the_exported_image_is_equal_to_IMAGE_2(
     driver,
 ):
@@ -94,5 +139,10 @@ def test_verify_with_image_comparison_whether_the_exported_image_is_equal_to_IMA
 
     img_diff = Image.new("RGBA", img1.size)
     mismatch = pixelmatch(img1, img2, img_diff)
-    assert mismatch == 0
     img_diff.save("IMAGE_1_diff.png")
+    allure.attach.file(
+        "IMAGE_1_diff.png",
+        name="Differences between IMG1 and IMG2",
+        attachment_type=attachment_type.PNG,
+    )
+    assert mismatch == 0
